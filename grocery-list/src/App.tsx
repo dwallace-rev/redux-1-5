e2beed722f40549cad69bb0da5058324740b3c93
dwallace-rev <82237381+dwallace-rev@users.noreply.github.com>
2022-01-05 19:12:55 -0500

    moving to laptop

--- grocery-list/src/App.tsx
+++ grocery-list/src/App.tsx
@@ -1,15 +1,24 @@
 import { Provider } from "react-redux";
-import BudgetPage from "./components/budget-page";
 import ToBuyPage from "./components/to-buy-page";
 import { groceryStore } from "./store";
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import BudgetPage from "./components/budget-page";
+import NavBar from "./components/nav-bar";
 
 export default function App() {
   return (<>
-    <Provider store={groceryStore}>
-      <h1>GroceryMate</h1>
-      {/* <BudgetPage/> */}
-      <ToBuyPage/>
-    </Provider>
+    
+      <BrowserRouter><Provider store={groceryStore}>
+        <h1>GroceryMate</h1>
+        <NavBar/>
+        <Routes>
+          <Route path="budget" element={<BudgetPage/>}/>          
+          <Route path="tobuy" element={<ToBuyPage/>}/>
+          <Route path="" element={<h1>Currently on the default page.</h1>}/>
+          
+        </Routes>
+      </Provider></BrowserRouter>
+    
   </>);
 }
 
